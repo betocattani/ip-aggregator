c1061b7ef14b282dbe83a22d8b9c3d83a36cf2c1
Cattani <contato.cattani@gmail.com>
2021-05-10 14:28:59 -0300

    refactor counter function

--- lib/ipaggregator/agregate.ex
+++ lib/ipaggregator/agregate.ex
@@ -1,8 +1,9 @@
 defmodule Ipaggregator.Agregate do
   def counter(ip_addresses) do
-    Enum.reduce(ip_addresses, %{}, fn ip, acc ->
-      count = acc[ip] || 0
-      Map.put(acc, ip, count + 1)
-    end)
+    Enum.reduce(ip_addresses, %{}, &increment/2)
+  end
+
+  defp increment(ip, map) do
+    Map.update(map, ip, 1, &(&1 + 1))
   end
 end
