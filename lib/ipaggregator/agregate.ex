efe7f412004ac4e40bde7a036d48e86153d3ed64
Cattani <contato.cattani@gmail.com>
2021-05-10 16:55:51 -0300

    implementing gen server to aggregate ips

--- lib/ipaggregator/agregate.ex
+++ lib/ipaggregator/agregate.ex
@@ -1,4 +1,29 @@
 defmodule Ipaggregator.Agregate do
+  use GenServer
+
+  @impl true
+  def init(ip_addresses), do: {:ok, ip_addresses}
+
+  @impl true
+  def handle_call(:counter, _from, ip_addresses) do
+    {:reply, __MODULE__.counter(ip_addresses), ip_addresses}
+  end
+
+  @impl true
+  def handle_cast({:enqueue_ips, value}, ip_addresses) do
+    {:noreply, ip_addresses ++ [value]}
+  end
+
+  def start_link(ip_addresses \\ []) do
+    GenServer.start_link(__MODULE__, ip_addresses, name: __MODULE__)
+  end
+
+  def counter do
+    GenServer.call(__MODULE__, :counter)
+  end
+
+  def enqueue_ips(value), do: GenServer.cast(__MODULE__, {:enqueue_ips, value})
+
   def counter(ip_addresses) do
     Enum.reduce(ip_addresses, %{}, &increment/2)
   end
