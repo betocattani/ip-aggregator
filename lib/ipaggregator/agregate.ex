137787e0a9ef0735557ed8a850b1127a082e874e
Cattani <contato.cattani@gmail.com>
2021-05-10 18:36:45 -0300

    add documentation and tests

--- lib/ipaggregator/agregate.ex
+++ lib/ipaggregator/agregate.ex
@@ -1,4 +1,28 @@
 defmodule Ipaggregator.Agregate do
+  @moduledoc """
+  This GenServer receives a list of IP addresses and iterates over the list
+  to count how many times the IP is called, an IP can be called 1 or N times.
+
+  ## Example
+      iex> ip_addresses = [
+            "1.2.3.4",
+            "1.2.3.4",
+            "3.4.5.6",
+            "10.1.0.38",
+            "90.37.182.241"
+          ]
+
+      iex> {:ok, pid} = GenServer.start_link(Ipaggregator.Agregate, ip_addresses)
+      {:ok, #PID<0.217.0>}
+
+      iex> GenServer.call(pid, :counter)
+      %{
+        "1.2.3.4" => 2,
+        "10.1.0.38" => 1,
+        "3.4.5.6" => 1,
+        "90.37.182.241" => 1
+      }
+  """
   use GenServer
 
   def start_link(ip_addresses \\ []) do
@@ -26,6 +50,27 @@ defmodule Ipaggregator.Agregate do
     {:noreply, ip_addresses ++ [value]}
   end
 
+  @doc """
+  This function is responsible to iterave over the list and count
+  how many times the IP is called.
+
+  ## Examples
+      iex> ip_addresses = [
+        "1.2.3.4",
+        "1.2.3.4",
+        "3.4.5.6",
+        "10.1.0.38",
+        "90.37.182.241"
+      ]
+
+      iex> Ipaggregator.Agregate.check_duplication(ip_addresses)
+      %{
+        "1.2.3.4" => 2,
+        "10.1.0.38" => 1,
+        "3.4.5.6" => 1,
+        "90.37.182.241" => 1
+      }
+  """
   def check_duplication(ip_addresses) do
     Enum.reduce(ip_addresses, %{}, &increment/2)
   end
--- test/ipaggregator_test.exs
+++ test/ipaggregator_test.exs
@@ -1,5 +1,5 @@
 defmodule IpaggregatorTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   doctest Ipaggregator
 
   test "returns a map of ip_addresses formated with the counter of each ip" do
@@ -27,10 +27,45 @@ defmodule IpaggregatorTest do
       "90.37.182.241" => 2
     }
 
-    assert Ipaggregator.Agregate.check_duplication(ip_addresses) == expected_output
+    {:ok, pid} = Ipaggregator.Agregate.start_link(ip_addresses)
+    assert GenServer.call(pid, :counter) == expected_output
+  end
+
+  test "enqueue a new IP in the list of addresses" do
+    ip_addresses = [
+      "1.2.3.4",
+      "3.4.5.6",
+      "10.1.0.38",
+      "90.37.182.241",
+      "10.12.15.0",
+      "90.37.182.241",
+      "172.10.11.15",
+      "111.111.17.35",
+      "3.4.5.6",
+      "172.16.28.99"
+    ]
+
+    expected_output = %{
+      "1.2.3.4" => 1,
+      "10.1.0.38" => 1,
+      "10.12.15.0" => 1,
+      "111.111.17.35" => 1,
+      "172.10.11.15" => 1,
+      "172.16.28.99" => 1,
+      "3.4.5.6" => 2,
+      "90.37.182.241" => 2,
+      "2.2.2.2" => 1
+    }
+
+    {:ok, pid} = Ipaggregator.Agregate.start_link(ip_addresses)
+    GenServer.cast(pid, {:enqueue_ip, "2.2.2.2"})
+
+    assert GenServer.call(pid, :counter) == expected_output
   end
 
   test "returns a empty map when ip_addresses list is empty" do
-    assert Ipaggregator.Agregate.check_duplication([]) == %{}
+    {:ok, pid} = Ipaggregator.Agregate.start_link([])
+
+    assert GenServer.call(pid, :counter) == %{}
   end
 end
